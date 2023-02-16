ab8956f2b2664471502e587c7fc32995a68130de
zjianzzhang <zjianzzhang@tencent.com>
2023-02-16 15:51:42 +0800

    fix(compile): change Git page update

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -49,7 +49,7 @@ const onLogin = async (formEl: FormInstance | undefined) => {
             // 获取后端路由
             initRouter().then(() => {
               router.push("/");
-              message("登录成功", { type: "success" });
+              message("登录成功！", { type: "success" });
             });
           }
         });
